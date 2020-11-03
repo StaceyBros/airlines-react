da94ccf938439d243a90b09f35fe152b956818c7
Farid118 <faridmasjedi118@gmail.com>
2020-11-03 22:44:12 +1100

    Update: APP.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@
 function App() {
   return (
     <div className="App">
-    <h1> HELLO TEAM </h1>
-
+      <h1> HELLO TEAM </h1>
+      <h2> I hope we do it </h2>
+      <h1> I am sure </h1>
     </div>
   );
 }
