da8dabf4120fcadba185fc907c5a29a88848fe71
JayOverall <jay.overall@hotmail.com>
2020-11-05 10:10:50 +1100

    Test 02

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
-
+import React from 'react';
+import Airplanes from './Airplanes';
+import Flights from './Flights'
+import SearchBar from './SearchBar';
 function App() {
   return (
     <div className="App">
-      <h1> HELLO TEAM </h1>
-      <h2> I hope we do it </h2>
-      <h1> I am sure </h1>
+        <Airplanes />
+        <Flights />
+        <SearchBar />
     </div>
   );
 }
-
 export default App;
